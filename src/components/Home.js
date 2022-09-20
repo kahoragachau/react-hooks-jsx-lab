d36b9d64e958835305d89ff7a471b36fa5db4286
kahoragachau <joelkahoragachau6@gmail.com>
2022-09-20 17:25:09 +0300

    - Done.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const firebrick = { color: "firebrick"}
+  return <div id="home">
+    <h1 style={firebrick}>{name} is a Web Developer from {city}</h1>
+  </div>;
 }
 
 export default Home;
